e5dc4a94bbf3e66f88e87eca237149c6ee2b0cda
jay <jayshomebrew+github@gmail.com>
2023-12-31 19:18:56 -0800

    updated.

--- duds/123movies_net.py
+++ duds/123movies_net.py
@@ -1,5 +1,11 @@
 # -*- coding: UTF-8 -*-
 
+"""
+ a lot of changes, lines 93 thru 113.
+ added def getlinks for readability.
+ Feel free to rewrite anything.
+"""
+
 import re
 import base64
 
@@ -10,13 +16,12 @@ from resources.lib.modules import cleantitle
 from resources.lib.modules import client
 from resources.lib.modules import client_utils
 from resources.lib.modules import scrape_sources
-#from resources.lib.modules import log_utils
-
+# from resources.lib.modules import log_utils
 
 class source:
     def __init__(self):
         self.results = []
-        self.domains = ['upmovies.to', '123movies.net', '0123putlocker.com', 'f123movies.com']
+        self.domains = ['upmovies.to']
         self.base_link = 'https://upmovies.to'
         self.search_link = '/search-movies/%s.html'
 
@@ -43,6 +48,36 @@ class source:
         return url
 
 
+    def getlinks(self, r):
+        newlinks = []
+        try:
+            r = client_utils.parseDOM(r, 'div', {'class': 'server_line'})
+            links = [(client_utils.parseDOM(i, 'a', ret='href')[0],
+                      client_utils.parseDOM(i, 'p', attrs={'class': 'server_servername'})[0]) for i in r]
+
+            for link in links:
+                try:
+                    host = re.findall('<a.*?>(.*?)</a>', link[1])
+                    host = host[0].lower()
+                    host = re.sub('server|link\s+\d+', '', host)
+                    host = client_utils.replaceHTMLCodes(host)
+
+                    if not host or 'other' in host:
+                        continue
+                    if 'vip' in host:
+                        host = 'eplayvid'
+                    if 'voesx' in host:
+                        host = 'voe'
+                    if 'aparat' in host:
+                        host = 'wolfstream'
+                    newlinks.append([link[0], host])
+                except:
+                    pass
+            return newlinks
+        except:
+            pass
+
+
     def sources(self, url, hostDict):
         try:
             if not url:
@@ -56,82 +91,54 @@ class source:
             search_term = '%s Season %s' % (title, season) if 'tvshowtitle' in data else title
             search_url = self.base_link + self.search_link % cleantitle.get_plus(search_term)
             html = client.scrapePage(search_url).text
-            r = client_utils.parseDOM(html, 'div', attrs={'class': 'ml-item'})
+
+            r = client_utils.parseDOM(html, 'div', attrs={'class': 'itemInfo'})
             if not r and 'tvshowtitle' in data:
                 search_url = self.base_link + self.search_link % cleantitle.get_plus(title)
                 html = client.scrapePage(search_url).text
-                r = client_utils.parseDOM(html, 'div', attrs={'class': 'ml-item'})
-            r = [(client_utils.parseDOM(i, 'a', ret='href'), re.findall('<b>Release:\s*(\d{4})</b>', i), re.findall('<b><i>(.+?)</i></b>', i)) for i in r]
+                r = client_utils.parseDOM(html, 'div', attrs={'class': 'itemInfo'})
+            r = [(client_utils.parseDOM(i, 'a', ret='href'), re.findall('Year:\s+(\d{4})', i), re.findall('<a.*?>(.*?)</a>', i)) for i in r]
             r = [(i[0][0], i[1][0], i[2][0]) for i in r if len(i[0]) > 0 and len(i[1]) > 0 and len(i[2]) > 0]
-            if 'tvshowtitle' in data:
-                try: # tv shows and cartoons.
-                    r = [(i[0], i[1], re.findall('(.+?) Season (\d+)$', client_utils.replaceHTMLCodes(i[2]))) for i in r]
-                    r = [(i[0], i[1], i[2][0]) for i in r if len(i[2]) > 0]
-                    url = [i[0] for i in r if cleantitle.match_alias(i[2][0], aliases) and cleantitle.match_year(i[1], year, data['year']) and i[2][1] == season][0]
-                    sepi = 'season-%1d/episode-%1d.html' % (int(season), int(episode))
-                    r = client.scrapePage(url).text
-                    r = client_utils.parseDOM(r, 'div', attrs={'id': 'details'})[0]
-                    r = client_utils.parseDOM(r, 'a', ret='href')
-                    url = [i for i in r if sepi in i][0]
-                except: # anime and some odd shows.
+            r = [(url, year, title.replace(',', ' ')) for url, year, title in r]
+            url = str()
+            if 'tvshowtitle' in data:  # tv shows and cartoons.
+                r = [(i[0], i[1], re.findall('(.+?) (?:SEASON|PART) (\d+)$', (i[2]), re.IGNORECASE)) for i in r]
+                r = [(i[0], i[1], i[2][0]) for i in r if len(i[2]) > 0]
+                url = [i[0] for i in r if cleantitle.match_alias(i[2][0], aliases) and cleantitle.match_year(i[1], year, data['year']) and i[2][1] == season][0]
+                r = client.scrapePage(url).text
+                r = client_utils.parseDOM(r, 'div', attrs={'id': 'details'})[0]
+                episode_url_list = client_utils.parseDOM(r, 'a', ret='href')
+                sepi = 'season-%1d/episode-%1d' % (int(season), int(episode))
+                sepipart = 'part-%1d/episode-%1d' % (int(season), int(episode))
+                patterns = [sepi, sepipart]
+                url = [url for url in episode_url_list if any(re.search(pattern, url) for pattern in patterns)][0]
+                if not url:  # anime and some odd shows.
                     url = [i[0] for i in r if cleantitle.geturl(title) in i[0] and cleantitle.match_year(i[1], year, data['year'])][0]
                     sepi = '/episode-%1d.html' % int(episode)
                     r = client.scrapePage(url).text
                     r = client_utils.parseDOM(r, 'div', attrs={'id': 'details'})[0]
                     r = client_utils.parseDOM(r, 'a', ret='href')
                     url = [i for i in r if sepi in i and not 'season-' in i][0]
-            else: # movies.
+                if not url:
+                    return self.results
+            else:  # movies.
                 r = [(i[0], i[1], re.findall('(.+?)(?:\(\d+\)|$)', client_utils.replaceHTMLCodes(i[2]))) for i in r]
                 r = [(i[0], i[1], i[2][0]) for i in r if len(i[2]) > 0]
                 url = [i[0] for i in r if cleantitle.match_alias(i[2], aliases) and cleantitle.match_year(i[1], year)][0]
-            r = client.scrapePage(url).text
-            try:
-                v = re.findall(r'document.write\(Base64.decode\("(.+?)"\)', r)[0]
-                b64 = base64.b64decode(v)
-                b64 = ensure_text(b64, errors='ignore')
-                try:
-                    link = client_utils.parseDOM(b64, 'iframe', ret='src')[0]
-                except:
-                    link = client_utils.parseDOM(b64, 'a', ret='href')[0]
-                if link:
-                    item = scrape_sources.make_item(hostDict, link, host=None, info=None, prep=True)
-                    if item:
-                        if not scrape_sources.check_host_limit(item['source'], self.results):
-                            self.results.append(item)
-            except:
-                #log_utils.log('sources', 1)
-                pass
-            try:
-                r = client_utils.parseDOM(r, 'div', {'class': 'server_line'})
-                links = [(client_utils.parseDOM(i, 'a', ret='href')[0], client_utils.parseDOM(i, 'p', attrs={'class': 'server_servername'})[0]) for i in r]
-                for link in links:
-                    try:
-                        host = re.sub('Server|Link\s*\d+', '', link[1]).lower()
-                        host = client_utils.replaceHTMLCodes(host)
-                        if not host or 'other' in host:
-                            continue
-                        if host == 'vip':
-                            host = 'eplayvid'
-                        if host == 'voesx':
-                            host = 'voe'
-                        if host == 'aparat':
-                            host = 'wolfstream'
-                        item = scrape_sources.make_item(hostDict, link[0], host=host, info=None, prep=True)
-                        if item:
-                            if scrape_sources.check_host_limit(item['source'], self.results):
-                                continue
-                            self.results.append(item)
-                    except:
-                        #log_utils.log('sources', 1)
-                        pass
-            except:
-                #log_utils.log('sources', 1)
-                pass
-            return self.results
-        except:
-            #log_utils.log('sources', 1)
-            return self.results
 
+            r = client.scrapePage(url).text
+            links = self.getlinks(r)
+            for link in links:
+                host = link[1]
+                item = scrape_sources.make_item(hostDict, link[0], host=host, info=None, prep=True)
+                if scrape_sources.check_host_limit(item['source'], self.results):
+                    continue
+                if item:
+                    self.results.append(item)
+
+        except:
+            # log_utils.log('sources', 1)
+            return self.results
 
     def resolve(self, url):
         if any(x in url for x in self.domains):
@@ -151,9 +158,8 @@ class source:
                 url = scrape_sources.prepare_link(url)
                 return url
             except:
-                #log_utils.log('resolve', 1)
+                # log_utils.log('resolve', 1)
                 pass
         else:
             return url
-
 
